2398e7955dfc5a8b1a6d512051839e25524ba241
Julian Ho <sscj2010@gmail.com>
2019-02-25 17:55:16 -0500

    created migrations for comments, likes, mentions

---
+++ db/migrate/20190225223152_create_likes.rb
@@ -0,0 +1,8 @@
+class CreateLikes < ActiveRecord::Migration[5.2]
+  def change
+    create_join_table :tweets, :users, table_name: :likes do |t|
+      t.index :tweet_id
+      t.index :user_id
+    end
+  end
+end
