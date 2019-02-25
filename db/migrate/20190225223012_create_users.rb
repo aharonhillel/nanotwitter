fca97e0313b9afa5794e1dee7abd1fd58da61234
Aaron Gold <aharonhillel@brandeis.edu>
2019-02-25 17:55:18 -0500

    Fix spelling mistake

--- db/migrate/20190225223012_create_users.rb
+++ db/migrate/20190225223012_create_users.rb
@@ -5,7 +5,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     t.string :email
     t.string :full_name
       t.date :dob
-    t.stirng :bio
+    t.string :bio
     t.date :api_token
     t.string :password
   end
