4700e0518823a8075e2856b7ffd08c0080ca98bc
Aaron Gold <aharonhillel@brandeis.edu>
2019-02-25 17:56:35 -0500

    Fix spelling mistake

--- db/migrate/20190225223853_create_tweets.rb
+++ db/migrate/20190225223853_create_tweets.rb
@@ -1,6 +1,6 @@
 class CreateTweets < ActiveRecord::Migration[5.2]
   def change
-    create_table :user do |t|
+    create_table :tweet do |t|
     t.integer :user_id
     t.integer :retweet_id
     t.text :content
