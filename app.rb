6163b362752959a1fa4fad8b8e74774d82978906
Julian Ho <sscj2010@gmail.com>
2019-02-26 16:54:29 -0500

    need fixing, auth does not work

--- app.rb
+++ app.rb
@@ -2,40 +2,40 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require 'bcrypt'
 require 'byebug'
-require_relative 'models/user'
 
-get "/signup" do
+require_relative '/models/user'
+
+get '/signup' do
   erb :"users/signup"
 end
 
-  post "/signup" do
-    user = User.new(:username => params[:username], :password => password(params[:password]), :email => params[:email])
-    if user.save
-      redirect_to "/login-sucessful"
-    else
-      redirect_to "/failure"
-    end
+post '/signup' do
+  user = User.new(username: params[:username], email: params[:email])
+  user.password = BCrypt::Password.create(params[:password])
+  if user.save
+    redirect '/login-successful'
+  else
+    redirect '/failure'
   end
+end
 
-  get "/login-successful" do
+get '/login-successful' do
   erb :"users/login-successful"
-  end
-
+end
 
 get '/login' do
-   erb :"users/login"
+  erb :"users/login"
 end
 
 post '/login' do
   byebug
-@user = User.find_by_email(params[:username])
- if @user.password == params[:password]
-   give_token
- else
-   redirect_to "/login-successful"
- end
+  @user = User.find_by_email(params[:username])
+  if @user.password == params[:password]
+    give_token
+  else
+    redirect '/login-successful'
+  end
 end
 
 get '/logout' do
-
 end
--- models/user.rb
+++ models/user.rb
@@ -4,15 +4,4 @@ class User < ActiveRecord::Base
   has_many :likes
   validates :username, :email, presence: true, uniqueness: true
   validates :password, presence: true
-
-  include BCrypt
-
-  def password
-    @password ||= Password.new(password)
-  end
-
-  def password=(new_password)
-    @password = Password.create(new_password)
-    self.password_hash = @password
-  end
 end
