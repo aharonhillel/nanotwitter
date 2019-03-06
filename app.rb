f46f14c34fed106c8de6e087fabbe10bcc419cba
irenezxr <xiangranzhao@outlook.com>
2019-03-06 18:59:09 -0500

    Add user controller

--- app.rb
+++ app.rb
@@ -4,49 +4,5 @@ require 'bcrypt'
 require 'byebug'
 require_relative 'models/user'
 require_relative 'controllers/tweet'
+require_relative 'controllers/user'
 
-enable :sessions
-
-get "/signup" do
-  erb :"users/signup"
-end
-
-post "/signup" do
-  @user = User.new(:username => params[:username], :email => params[:email])
-  @user.password = params[:password]
-  if @user.save
-    session[:user_id] = @user.id
-    redirect '/homepage'
-    #erb :"users/login-successful"
-  else
-    redirect_to "/failure"
-  end
-end
-
-get '/homepage' do
-  @user = User.find(session[:user_id])
-  erb :'homepage'
-end
-
-
-get '/login' do
-   erb :"users/login"
-end
-
-post '/login' do
-  #byebug
-  @user = User.find_by_email(params[:email])
-  if @user != nil && @user.password == params[:password]
-    "Logged in"
-    redirect '/homepage'
-    #Need to write give_token function
-     #give_token
-  else
-    "Wrong"
-  # redirect_to "/login-successful"
- end
-end
-
-get '/logout' do
-
-end
---
+++ controllers/user.rb
@@ -0,0 +1,45 @@
+enable :sessions
+
+get '/signup' do
+  erb :'users/signup'
+end
+
+post '/signup' do
+  @user = User.new(:username => params[:username], :email => params[:email])
+  @user.password = params[:password]
+  if @user.save
+    session[:username] = @user.username
+    redirect '/users/' + session[:username]
+  else
+    "Failed"
+    #redirect_to "/failure"
+  end
+end
+
+get '/users/:username' do
+  @user = User.find_by_username(session[:username])
+  erb :'users/homepage'
+end
+
+
+get '/login' do
+  erb :'users/login'
+end
+
+post '/login' do
+  #byebug
+  @user = User.find_by_email(params[:email])
+  if @user != nil && @user.password == params[:password]
+    "Logged in"
+    erb :'users/homepage'
+    #Need to write give_token function
+    #give_token
+  else
+    "Wrong"
+    # redirect_to "/login-successful"
+  end
+end
+
+get '/logout' do
+
+end
