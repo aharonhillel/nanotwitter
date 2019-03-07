1ded88c120100a7f448221310a4d288532bc296c
Aaron Gold <aharonhillel@brandeis.edu>
2019-03-07 13:51:10 -0500

    Added more user tests

--- test/test_follows.rb
+++ test/test_follows.rb
@@ -26,22 +26,8 @@ describe 'POST on /follows/:followed' do
     assert_equal user2.following.count, 0
     assert_equal user1.followers.count, 0
     assert_equal user2.followers.count, 1
-  end
-  it 'Cant follow yourself' do
-    browser = Rack::Test::Session.new(Rack::MockSession.new(Sinatra::Application))
-
-    user1 = create_dummy_user(1)
-    user2 = create_dummy_user(2)
-    post '/follows/user1',{}, 'rack.session' => { :user =>user1 }
-    last_response.ok?
-    assert_equal "Cannot follow yourself", last_response.body
-  end
-end
-
-describe 'POST on /follows/:followed' do
-  before do
-    Follow.delete_all
-    User.delete_all
+    assert_equal user1.following.first.id, user2.id
+    assert_equal user2.followers.first.id, user1.id
   end
   it 'Cant follow yourself' do
     browser = Rack::Test::Session.new(Rack::MockSession.new(Sinatra::Application))
@@ -57,6 +43,7 @@ describe 'POST on /follows/:followed' do
     assert_equal user2.followers.count, 0
   end
 end
+
 
 
 def create_dummy_user(id)
