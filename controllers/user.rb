425623fe62255b406cbee6c88ad1d781f3ebf493
irenezxr <xiangranzhao@outlook.com>
2019-03-06 22:52:26 -0500

    List all followers

--- controllers/user.rb
+++ controllers/user.rb
@@ -39,8 +39,9 @@ end
 post '/login' do
   #byebug
   @user = User.find_by_email(params[:email])
-  session[:username] = @user.username
+
   if @user != nil && @user.password == params[:password]
+    session[:username] = @user.username
     "Logged in"
     erb :'users/homepage'
     #Need to write give_token function
@@ -75,6 +76,7 @@ get '/users/:username/followers' do
   if @user.nil?
     "#{params[:username]} has no followers"
   else
-    erb :'follows/following'
+    erb :'follows/followers'
   end
 end
+
