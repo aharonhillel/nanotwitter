3fbfd82d60f1085d80a55855ccf06cb426a75c21
irenezxr <xiangranzhao@outlook.com>
2019-04-01 02:31:52 -0400

    Add test files to test.rb

--- test/test.rb
+++ test/test.rb
@@ -3,6 +3,9 @@ require_relative 'test_users'
 require_relative 'test_follows'
 require_relative 'test_likes'
 require_relative 'test_comments'
+require_relative 'test_mentions'
+require_relative 'test_hash_tags'
+require_relative 'test_models'
 
 
 
