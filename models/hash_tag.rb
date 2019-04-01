6b822db23d1c10fbae24aa52b527c28f5a6edd35
irenezxr <xiangranzhao@outlook.com>
2019-03-31 20:50:05 -0400

    Model testing for hashtag and hashtag_tweet. All tests passed.

--- models/hash_tag.rb
+++ models/hash_tag.rb
@@ -1,4 +1,6 @@
 class HashTag < ActiveRecord::Base
-  has_many :hash_tag_tweets
+  has_many :hash_tag_tweets, dependent: :nullify
   has_many :tweets, through: :hash_tag_tweets
+
+  validates :description, uniqueness: true, presence: true, length: {maximum: 20}
 end
