fabf60911f5af81712959d112d48e9ba1c755148
Julian Ho <sscj2010@gmail.com>
2019-04-18 16:00:53 -0400

    roll back to stdlib http client

--- vendor/dgraph/dgraph.rb
+++ vendor/dgraph/dgraph.rb
@@ -1,33 +1,48 @@
 require 'json'
 require 'faraday'
 require 'net/http/persistent'
+require 'net/http'
 
 class Dgraph
   class Client
     def initialize(options = {})
-      @conn = Faraday.new(:url => "http://#{options[:host]}:#{options[:port]}") do |faraday|
-        # faraday.response :logger                  # log requests and responses to $stdout
-        faraday.adapter  Faraday.default_adapter
-      end
+      # @conn = Faraday.new(:url => "http://#{options[:host]}:#{options[:port]}") do |faraday|
+      #       #   # faraday.response :logger                  # log requests and responses to $stdout
+      #       #   faraday.adapter  Faraday.default_adapter
+      #       # end
+      @client = Net::HTTP.new(options[:host], options[:port])
     end
 
     def alter(options = {})
-      res = @conn.post do |req|
-        req.url '/alter'
-        req.headers['Content-Type'] = 'application/json'
-        req.body = options[:schema]
-      end
+      # res = @conn.post do |req|
+      #   req.url '/alter'
+      #   req.headers['Content-Type'] = 'application/json'
+      #   req.body = options[:schema]
+      # end
+      req = Net::HTTP::Post.new('/alter')
+      req['accept'] = 'application/json'
+      # req.continue_timeout = options[:timeout]
+      req.body = options[:schema]
+
+      res = @client.request(req)
 
       body = JSON.parse(res.body, symbolize_names: true)
       body.dig(:data, :code).equal? 'Success'
     end
 
     def query(options = {})
-      res = @conn.post do |req|
-        req.url '/query'
-        req.headers['Content-Type'] = 'application/json'
-        req.body = options[:query]
-      end
+      # res = @conn.post do |req|
+      #   req.url '/query'
+      #   req.headers['Content-Type'] = 'application/json'
+      #   req.body = options[:query]
+      # end
+
+      req = Net::HTTP::Post.new('/query')
+      req['accept'] = 'application/json'
+      # req.continue_timeout = options[:timeout]
+      req.body = options[:query]
+
+      res = @client.request(req)
 
       if options[:raw]
         JSON.parse(res.body, symbolize_names: true)
@@ -37,12 +52,20 @@ class Dgraph
     end
 
     def mutate(options = {})
-      res = @conn.post do |req|
-        req.url '/mutate'
-        req.headers['Content-Type'] = 'application/json'
-        req.headers['X-Dgraph-CommitNow'] = true
-        req.body = options[:query]
-      end
+      # res = @conn.post do |req|
+      #   req.url '/mutate'
+      #   req.headers['Content-Type'] = 'application/json'
+      #   req.headers['X-Dgraph-CommitNow'] = true
+      #   req.body = options[:query]
+      # end
+
+      req = Net::HTTP::Post.new('/mutate')
+      req['accept'] = 'application/json'
+      # commit immediately
+      req['X-Dgraph-CommitNow'] = true
+      req.body = options[:query]
+
+      res = @client.request(req)
 
       body = JSON.parse(res.body, symbolize_names: true)
 
